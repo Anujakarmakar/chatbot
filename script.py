853f69a5075865a9f88f43c6650b8a019eff281f
= <=>
2024-11-10 15:05:52 +0530

    db connection establish

---
+++ script.py
@@ -0,0 +1,21 @@
+import psycopg
+
+conn_info = "dbname=College user=postgres password=AnujaPostgres444 host=localhost port=5432"
+
+conn = psycopg.connect(conn_info)
+cur = conn.cursor()
+
+# question = intput(_)
+# query = gemini.get_response(context + question)
+
+chat = '''
+user: context question
+gpt: query
+'''
+
+# cur.execute(query)
+# rows = cur.fetchall()
+
+# ans = gemin.get_response(chat + rows)
+
+# print(ans)
